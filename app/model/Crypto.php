012feef391280c9e60f69e866cd4483b3ac4a987
gatch14 <gatch14@gmail.com>
2017-12-03 17:02:14 +0100

    add crypto model

--- app/model/Crypto.php
+++ app/model/Crypto.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Crypto extends Model
 {
-    //
+    protected $table = 'cryptos';
+
+    protected $fillable = ['name', 'price', 'choices', 'choices_value', 'alerted', 'user_id'];
 }
